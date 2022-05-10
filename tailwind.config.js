6583642fdb0fd5ade028adcf028422f5248e1a9d
gustavo-gava <gustavogava123@hotmail.com>
2022-05-10 09:01:58 -0300

    feat: added global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 module.exports = {
 	content: ["./src/**/*.tsx"],
+	darkMode: "class",
 	theme: {
 		extend: {
 			colors: {
 				brand: {
 					500: "#8257E6",
+					hover: "#996DFF",
+					"text-on-brand-color": "#FFFFFF",
 				},
+				background: {
+					dark: "#09090A",
+					light: "#FEFEFE",
+				},
+				surface: {
+					primary: {
+						dark: "#18181B",
+						light: "#FFFFFF",
+					},
+					secondary: {
+						dark: "#27272A",
+						light: "#F4F4F5",
+					},
+				},
+				text: {},
 			},
 		},
 	},
