890f41075e49285af87f840d6724869799ad475f
gustavo-gava <gustavogava123@hotmail.com>
2022-05-17 07:32:35 -0300

    feat(component): added ScreenshotButton component

---
+++ src/components/ScreenshotButton/index.tsx
@@ -0,0 +1,48 @@
+import html2canvas from "html2canvas"
+import { Camera } from "phosphor-react"
+import { useState } from "react"
+import { Loading } from "../Loading"
+
+interface ScreenshotButtonProps {
+	onScreenshotTook: (photo: string) => void
+	screenshot: string | null
+}
+
+export function ScreenshotButton({ onScreenshotTook, screenshot }: ScreenshotButtonProps) {
+	const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
+
+	async function handleTakeScreenshot() {
+		try {
+			setIsTakingScreenshot(true)
+
+			const canvas = await html2canvas(document.querySelector("html")!)
+			const base64image = canvas.toDataURL("image/png")
+
+			onScreenshotTook(base64image)
+		} catch (err) {
+			alert("Não foi possível tirar a screenshot")
+			console.error(err)
+		} finally {
+			setIsTakingScreenshot(false)
+		}
+	}
+
+	if (screenshot) {
+		return (
+			<button
+				type="button"
+				className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end"
+			></button>
+		)
+	}
+
+	return (
+		<button
+			className="bg-surface-secondary-dark border-transparent p-2 rounded-lg hover:bg-surface-secondary-hover-dark transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+			onClick={handleTakeScreenshot}
+			type="button"
+		>
+			{isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6 text-text-primary-dark" />}
+		</button>
+	)
+}
